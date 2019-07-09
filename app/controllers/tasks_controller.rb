d029c571f2645fa0b07ad535c07271c43fefdfba
Alexander Rupp-Coppi <amr2311@columbia.edu>
2019-07-09 15:52:38 +0200

    working list

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,38 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def submit_new
+    Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def submit_edit
+    Task.update(params[:id], task_params)
+    redirect_to tasks_path
+  end
+
+  def remove
+    Task.delete(params[:id])
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    return params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index', as: 'tasks'
+
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  post 'tasks/new', to: 'tasks#submit_new', as: 'submit_new_task'
+  
+  get 'tasks/edit/:id', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/edit/:id', to: 'tasks#submit_edit', as: 'submit_edited_task'
+
+  get 'tasks/remove/:id', to: 'tasks#remove', as: 'remove_task' 
+  
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
 end
